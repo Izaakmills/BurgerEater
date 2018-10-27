faeedf53becbe53ccfc0012b99ca06af4761fffc
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-10-26 20:12:54 -0400

    updates

---
+++ public/js/logic.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("newBurger").addEventListener("onClick", function () {
+        console.log("son of a bitch")
+    })
+})
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("./app/public"));
+app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,16 +15,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./app/routes/htmlroutes.js");
+const routes = require("./app/routes/htmlroutes")
 
-app.use(routes);
+app.use(routes)
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
